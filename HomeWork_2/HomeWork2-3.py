082aee47f085a4db686fd15c27e3a4cb994e3252
Unter <unter83@gmail.com>
2022-12-23 19:57:35 +0300

    Made task 2-3

---
+++ HomeWork_2/HomeWork2-3.py
@@ -0,0 +1,23 @@
+print("Программа находит произведение чисел, которые указываются в виде индексов, из последовательности -N, N")
+
+num = int(input("Введите число N: "))
+
+li = list(range(-num, num + 1))
+
+print(li)
+
+print("Введите индексы массива, разделяя их пробелом. У первого элемента массива индекс 1.")
+indexes = input().split()
+int_indexes = list(map(int, indexes))
+
+print(int_indexes)
+prod = 1
+for i in int_indexes:
+    if i > (2 * num + 1) or i <= 0:
+        print("Укажите индкесы в допустимом диаазоне")
+        exit()
+    print(li[i - 1])
+    prod = prod * li[i - 1]
+
+print (f"Произвдение чисел {prod}")
+
