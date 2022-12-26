92bb948efd178640ae6ff0218540395f1db093cf
Unter <unter83@gmail.com>
2022-12-26 20:49:31 +0300

    finish task 3-2

---
+++ HomeWork_3/HomeWork3-2.py
@@ -0,0 +1,23 @@
+import random
+
+def CreateRandomArray(list_limit, min, max: int) -> list: 
+    list_of_num = []
+    for i in range(0, list_limit):
+        list_of_num.append(random.randint(min, max))
+    return list_of_num
+
+print("Программа которая находит произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.\n")
+
+list_limit = int(input("Введите длину списка случайных чисел: "))
+min = int(input("Введите минимальное число случайной последовательности: "))
+max = int(input("Введите максимальное число случайной последовательности: "))
+
+list_of_num = CreateRandomArray(list_limit, min, max)
+print(list_of_num)
+print()
+
+print("Произведение пар:")
+for i in range(0, list_limit // 2):
+    # k = list_limit - i
+    # print(i)
+    print(f"{list_of_num[i]} * {list_of_num[list_limit - i - 1]} = {list_of_num[i]*list_of_num[list_limit - i - 1]}")
