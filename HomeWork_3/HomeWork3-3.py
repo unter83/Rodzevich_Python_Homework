d1c371309f82ec92fdc4fff51317d2329f9a5bf3
Unter <unter83@gmail.com>
2022-12-26 21:24:13 +0300

    finish task 3-3

--- HomeWork_3/HomeWork3-3.py
+++ HomeWork_3/HomeWork3-3.py
@@ -1,4 +1,4 @@
-import random, math
+import random
 
 def CreateRandomFloatArray(list_limit, min, max: int) -> list: 
     list_of_num = []
@@ -6,7 +6,7 @@ def CreateRandomFloatArray(list_limit, min, max: int) -> list:
         list_of_num.append(round(random.random() + random.randint(min, max), 2))
     return list_of_num 
 # random.randint(min, max) + 
-print("Программа которая найдёт разницу между максимальным и минимальным значением дробной части элементов списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.\n")
+print("Программа которая найдёт разницу между максимальным и минимальным значением дробной части элементов списка.\n")
 
 list_limit = int(input("Введите длину списка случайных чисел: "))
 min = int(input("Введите минимальное число случайной последовательности: "))
