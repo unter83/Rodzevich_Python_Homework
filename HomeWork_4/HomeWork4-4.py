05aac274a517fa856e9938cfd653ffac97a9dee5
Unter <unter83@gmail.com>
2023-01-04 15:37:47 +0300

    finish task 4-4

---
+++ HomeWork_4/HomeWork4-4.py
@@ -0,0 +1,54 @@
+import random
+
+print("Программа формирует случайный многочлен степени K\n")
+
+num = int(input("Введите степень многочлена: "))
+i = num
+start = False
+while i >= 0:
+
+    if i > 1 and start == False:
+        k = random.randint(0, 101)
+        
+        if k > 1:        
+            print(f"{k}*x^{i}", end="")
+            start = True
+        elif k == 1:
+            print(f"x^{i}", end="")
+            start = True
+
+    elif i == 1 and start == False:
+        k = random.randint(0, 101)
+        
+        if k > 1:       
+            print(f"{k}*x" , end="")
+            start = True
+        elif k == 1:
+            print(f"x" , end="")
+            start = True
+
+    elif i == 0 and start == False:
+        k = random.randint(0, 101)
+        
+        if k > 1:       
+            print(f"{k}" , end="")
+            start = True
+    
+    if i > 1 and start == True:
+        k = random.randint(0, 101)
+        if k > 1:        
+            print(f" + {k}*x^{i}", end="")
+        elif k == 1:
+            print(f" + x^{i}", end="")
+    elif i == 1:
+        k = random.randint(0, 101)
+        if k > 1:       
+            print(f" + {k}*x" , end="")
+        elif k == 1:
+            print(f" + x" , end="")
+    elif i == 0:
+        k = random.randint(0, 101)
+        if k >= 1:       
+            print(f" + {k}" , end="")
+
+    i -= 1
