3127645601b962f304ceed842e0151ec0ce70b8e
Unter <unter83@gmail.com>
2023-01-18 16:20:30 +0300

    end task 5-1

--- HomeWork_5/HomeWork_5_1.py
+++ HomeWork_5/HomeWork_5_1.py
@@ -1,56 +1,77 @@
 
 import random
 
-print("Игра в конфеты\n")
+print("\n*** Игра в конфеты ***\n")
 print("На столе лежит 120 конфет. Играют два игрока делая ход друг после друга.\nПервый ход делает человек. За один ход можно забрать не более чем 28 конфет.\n")
 
-# def Player_Take_Candy() -> int:
-#     candy_taken = int(input("Возьмите от 1 до 28 конфет: "))
-#     if (candy_taken > 0 and candy_taken <= 28):
-#         return candy_taken
-#     else:
-#         Player_Take_Candy()
+# def ToDigit()
 
-def Player1_Takes_Candy() -> int:
+def Player1_Takes_Candy(candy_pool) -> int:
     player1_takes_candy = False    
     while (not player1_takes_candy):
-        candy_taken = int(input("Возьмите от 1 до 28 конфет: "))
-        
-        if (candy_taken > 0 and candy_taken <= 28):
-            player1_takes_candy = True
+        candy_taken = input("Возьмите от 1 до 28 конфет: ")
+        if candy_taken.isdigit():
+            candy_taken = int(candy_taken)
+            if (candy_taken > 0 and candy_taken <= 28):
+                if candy_pool >= candy_taken:
+                    print(f"Игрок берёт {candy_taken} конфет\n")
+                    player1_takes_candy = True
+                else:
+                    candy_taken = candy_pool
+                    print(f"Игрок берёт {candy_taken} конфет\n")
+                    player1_takes_candy = True  
+            else:
+                print("Вы можете взять от 1 до 28 конфет")
         else:
-            print("Вы можете взять от 1 до 28 конфет")
-
+            print("Введите число")
     return candy_taken
 
-def Player_Bot_Takes_Candy() -> int:
+def Bot_Takes_Candy(candy_pool) -> int:
     candy_taken = random.randint(1,29)
-    print(f"Компьютер берёт {candy_taken} конфет")
+
+    if candy_pool >= candy_taken:
+        print(f"Компьютер берёт {candy_taken} конфет\n")
+    else:
+        candy_taken = candy_pool
+        print(f"Компьютер берёт {candy_taken} конфет\n")
+
     return candy_taken
+
+def ShowCandyPool(candy_pool):
+    print(f"На столе лежит {candy_pool} конфет \n")
+   
+def IsVictory(candy_pool):
+    if candy_pool <= 0:
+        return True
+    else:
+        return False
 
 candy_pool = 120
 player1_candy = 0
 player_bot_candy = 0
 candy_taken = 0
-
-print(f"candy - {candy_pool}")
-print(f"player1_candy - {player1_candy}")
-print()
+roll = 0
 
 while (candy_pool > 0):
-
-    candy_taken = Player1_Takes_Candy()
+    roll += 1
+    print(f"-Раунд {roll}-\n")
+    candy_taken = Player1_Takes_Candy(candy_pool)
     candy_pool -= candy_taken
     player1_candy += candy_taken
-    print(f"candy - {candy_pool}")
-    print(f"player1_candy - {player1_candy}")
-    print()
 
-    candy_taken = Player_Bot_Takes_Candy()
+    if IsVictory(candy_pool):
+        print(f"Победил игрок\n")
+        break
+
+    candy_taken = Bot_Takes_Candy(candy_pool)
     candy_pool -= candy_taken
     player_bot_candy += candy_taken
-    print(f"candy - {candy_pool}")
-    print(f"player_bot_candy - {player_bot_candy}")
-    print()
+    ShowCandyPool(candy_pool)
+
+    if IsVictory(candy_pool):
+        print(f"Победил компьютер\n")
+        break
+
+
     
 
