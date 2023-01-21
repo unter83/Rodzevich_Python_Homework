2c30ec1e28879a051bcdac1ae35c39288d35090e
Unter <unter83@gmail.com>
2023-01-21 12:42:25 +0300

    finish task 5-4

--- HomeWork_5/HomeWork_5_4.py
+++ HomeWork_5/HomeWork_5_4.py
@@ -1,30 +1,65 @@
-print("Программа реализует RLE алгоритм для сжатия и раскрытия строк\n\n")
 
-original_string = input("Введите строку для сжатия: ")
-# original_string = original_string  + "1"
-print(original_string)
-count = 0
-li_string = []
-flag = False
-temp = original_string[0]
-for i in original_string:
-    # 1. i := a; 2. i := a;  3. i := s;  4. i := s; 5. i := d; 
 
-    if i == temp: # 1. да; 2. да; 3. нет; 4. да
-        count += 1  #  1. count := 1; 2. count := 2; 4. count := 1;
-    else:
-        flag = True
-        
-    if flag:
-        if count == 1: 
-            li_string.append(temp)
-            count = 0
-            emp = i 
-        elif count > 1: # 3. да;
-            li_string.append(str(count) + temp) # 3. '2a';
-            count = 0   # 3. count := 0;
-            temp = i   # 3. temp = s;
-        
+def ZipString(original_string) -> str:
+    flag = 0
+    count = 0
+    new_string = ""
+    temp = original_string[0]
+    for i in original_string:
+        flag += 1
+        if i == temp:
+            count += 1
+        else:
+            if count == 1:
+                new_string = new_string + temp
+                count = 1
+                temp = i
+            elif count > 1:
+                new_string = new_string + str(count) + temp
+                count = 1
+                temp = i
+        if flag == len(original_string):
+            if count == 1:
+                new_string = new_string + temp
+                count = 1
+                temp = i
+            elif count > 1:
+                new_string = new_string + str(count) + temp
+                count = 1
+                temp = i
+    return new_string
+
+def UnzipString(zip_string) -> str:
+    unzip_string = ""
+    digit_num = 0
+    digit_string = ""
+    for i in zip_string:
+        if str(i).isdigit():
+            digit_num += 1    
+            digit_string += i
+        else:
+            if digit_num > 0:
+                for k in range(int(digit_string)):
+                    unzip_string += i
+            else:
+                unzip_string += i
+            digit_string = ""
+            digit_num = 0
+
+    return unzip_string
+
+def main():
+    print("Программа реализует RLE алгоритм для сжатия и раскрытия строк\n\n")
+
+    original_string = input("Введите строку для сжатия: ")
+    print()
+    print(f"Исходная строка стока - {original_string}\n")
+    new_string = ZipString(original_string) 
+    print(f"Архивированная стока - {new_string}\n")
+    unzip_string = UnzipString(new_string)   
+    print(f"Разархивированная стока - {unzip_string}\n")
+
+main()
 # count = 0
 # li_string = []
 # letters_counter = 1
@@ -47,4 +82,3 @@ for i in original_string:
 #             letters_counter = 1
 #     count += 1
 
-print(li_string)
