df61e648869c3e050b448f560ee5c50bf43dd933
Unter <unter83@gmail.com>
2023-01-28 15:16:45 +0300

    made task6-2

---
+++ HomeWork_6/HomeWorl_6_2.py
@@ -0,0 +1,8 @@
+print("Программа делит список на 2 списка состоящих из чисел и строк\n")
+
+lst = list(map(str, input("Введите числа, разделяя их проблеми: ").split()))
+lst_2 = [0]*2
+
+lst_2[0]= list(filter(lambda i: i.isdigit(), lst))
+lst_2[1]= list(filter(lambda i: not i.isdigit(), lst))
+print(lst_2)
